bc416fe5432ce5033aa56a393ac05cedfebdb098
rwyble <Rich.Wyble@VeteransUnited.com>
2021-03-24 06:35:28 -0700

    adding file

---
+++ get-record.py
@@ -0,0 +1,30 @@
+# Copyright 2019 Cloudera, Inc.
+# Not to be reproduced or shared without prior written consent from Cloudera.
+
+
+def get_state_temp(one_record):
+    """This function retrieves the state of radios and temp from one record"""
+    pass
+
+
+def display_state_temp(bluetooth, wifi, dev_temp):
+    # Determine state of radios
+    pass
+
+
+if __name__ == "__main__":
+    """This program displays the state of radios and temp from a log file
+
+    Parameters
+    ----------
+    file_location (argv[1]): which file to read
+    which_record (argv[2]): the line that contains the record we want 
+    """
+
+    # Set the variables
+
+    # Open file and get the n-th line
+
+    # Get the record and values
+
+    # Display the state and temp
