d3e8f3f36506968595b63e03a46defbdbe12d9d6
rwyble <Rich.Wyble@VeteransUnited.com>
2021-04-01 12:33:31 -0700

    wtf

--- get-record.py
+++ get-record.py
@@ -2,6 +2,7 @@
 # Not to be reproduced or shared without prior written consent from Cloudera.
 
 
+
 def get_state_temp(one_record):
     """This function retrieves the state of radios and temp from one record"""
     pass
@@ -11,6 +12,9 @@ def display_state_temp(bluetooth, wifi, dev_temp):
     # Determine state of radios
     pass
 
+import processlog as pl
+
+
 
 if __name__ == "__main__":
     """This program displays the state of radios and temp from a log file
@@ -18,13 +22,22 @@ if __name__ == "__main__":
     Parameters
     ----------
     file_location (argv[1]): which file to read
-    which_record (argv[2]): the line that contains the record we want 
+
+    which_record (argv[2]): the line that contains the record we want
     """
 
     # Set the variables
+    file_location = sys.argv[1]
+    which_record = int(sys.argv[2])
 
     # Open file and get the n-th line
+    with open(file_location, 'r') as log_file:
+        lines = log_file.readlines()
 
     # Get the record and values
+    record = lines[which_record]
+    values = pl.get_state_temp(record)
 
     # Display the state and temp
+    pl.display_state_temp(values[0], values[1], values[2])
+
